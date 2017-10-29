2adff4d543f8cdde3f1624a992ce99164c866b6b
Bhavya Sruthi Sode <sbhavyasruthi@gmail.com>
2017-10-30 02:51:25 +0530

    Uploading files which demonstrates the concept of module-dependencies in Angular.

---
+++ Module Dependencies Example/app.js
@@ -0,0 +1,8 @@
+var mod = angular.module("dependencies",["userMod"]);
+    
+mod.controller("dependenciesCntrl",dependenciesCntrl);
+    function dependenciesCntrl()
+    {
+        //alert("utkt");
+         this.hi = "hellooo" ;
+    }
---
+++ Module Dependencies Example/user.js
@@ -0,0 +1,6 @@
+angular.module("userMod",[]).controller("userCntrl",userCntrl);
+    function userCntrl()
+    {
+         this.uname = "sruthi" ;
+         this.hi =" hi sru";
+    }
