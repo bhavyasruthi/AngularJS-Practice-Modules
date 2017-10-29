5297411af9b82f01afa50af5524fcc370217b666
Bhavya Sruthi Sode <sbhavyasruthi@gmail.com>
2017-10-30 02:48:48 +0530

    Uploading files which displays Time using $scope in angular.

---
+++ Time App/app.js
@@ -0,0 +1,21 @@
+
+var mmodule = angular.module("clock-mod",[]);
+    
+mmodule.controller("clock-cntrl",displayTime);
+    function displayTime($scope)
+    {
+        
+        //$scope.ftime = "12" + ""+new Date().getDate();;
+        $scope.cTime= new Date().toTimeString();
+        $scope.hi = function()
+        {
+            alert("hiiii"+$scope.name);
+            //ftime="hi mine";
+        }
+        $scope.updateTime = function()
+        {
+            alert("updated");
+            $scope.cTime= new Date().toTimeString();
+        }
+        //console.log("kjnikj"+updateTime());
+    }
