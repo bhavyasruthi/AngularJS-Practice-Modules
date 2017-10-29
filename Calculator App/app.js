bdc268720d094918046fdfe1411f838f8a2b8620
Bhavya Sruthi Sode <sbhavyasruthi@gmail.com>
2017-10-30 02:34:34 +0530

    Uploading basic calculator app using angular from local computer to Github.

---
+++ Calculator App/app.js
@@ -0,0 +1,25 @@
+
+var mod = angular.module("cal-mod",[]);
+    
+mod.controller("cal-cntrl",calcntrl);
+    function calcntrl()
+    {
+        
+            console.log(" changed");
+            this.showResult = function()
+        {
+           // var anum =  parseFloat(this.a);
+           var anum = this.a;
+            var bnum =  parseFloat(this.b);
+            if(this.opt === '+')
+            this.c = anum + bnum;
+            else if(this.opt === '-')
+            this.c = anum - bnum;
+             else if(this.opt === '*')
+            this.c = anum * bnum;
+             else if(this.opt === '/')
+            this.c = anum / bnum;
+            alert("button pressed"+this.a + anum +this.c);
+        }
+    }
+    
