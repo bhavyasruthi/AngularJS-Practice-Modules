23be142f668a1220d4be5a26093a31a30350f578
Bhavya Sruthi Sode <sbhavyasruthi@gmail.com>
2018-01-08 16:33:06 -0500

    Update app.js

--- Module Dependencies Example/app.js
+++ Module Dependencies Example/app.js
@@ -3,5 +3,5 @@ var mod = angular.module("dependencies",["userMod"]);
 mod.controller("dependenciesCntrl",dependenciesCntrl);
     function dependenciesCntrl()
     {
-         this.hi = "hellooo, I am from UserMod module" ;
+         this.hi = "hellooo, I am from dependencies module" ;
     }
